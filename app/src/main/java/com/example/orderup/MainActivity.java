112c597bfb3292b0cbc759d8bc2d7cb706506abd
elbth <elizabethchin@live.com>
2020-09-19 14:27:12 -0700

    food categories page

--- app/src/main/java/com/example/orderup/MainActivity.java
+++ app/src/main/java/com/example/orderup/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.loginButton);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // click on login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                setContentView(R.layout.activity_main2);
+                // move to food/time choice page
+                setContentView(R.layout.filter_choice);
+                // click on food button
+                Button foodButton = (Button) findViewById(R.id.foodButton);
+                foodButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        setContentView(R.layout.food_sort);
+                    }
+                });
+                // click on time button
+                Button timeButton = (Button) findViewById(R.id.timeButton);
+                timeButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        setContentView(R.layout.activity_main);
+                    }
+                });
             }
         });
+        //TextView welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
+        //welcomeTextView.setText("hello hello");
     }
 }
