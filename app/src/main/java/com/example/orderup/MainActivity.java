3ef57ec959116300f12a782cf47e16c6e0d96e41
PureWasian <alancooper509@hotmail.com>
2020-09-19 20:44:15 -0500

    added functions to mainactivity, updated ids on food_sort.xml

--- app/src/main/java/com/example/orderup/MainActivity.java
+++ app/src/main/java/com/example/orderup/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.orderup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,40 +26,66 @@ public class MainActivity extends AppCompatActivity {
         Button loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // move to food/time choice page
-                setContentView(R.layout.filter_choice);
-                // click on food button
-                Button foodButton = (Button) findViewById(R.id.foodButton);
-                foodButton.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        // move to food choices page
-                        setContentView(R.layout.food_sort);
-                        // add restaurant info to each restaurant
-                        TextView restOne = (TextView) findViewById(R.id.restOne);
-                        restOne.setText("hello hello doo doo doo");
-
-                        TextView restTwo = (TextView) findViewById(R.id.restTwo);
-                        restTwo.setText("beeboop");
-
-                        TextView restThree = (TextView) findViewById(R.id.restThree);
-                        restThree.setText("yeehaw");
-
-                        TextView restFour = (TextView) findViewById(R.id.restFour);
-                        restFour.setText("bing bong");
-
-                        TextView restFive = (TextView) findViewById(R.id.restFive);
-                        restFive.setText("achoo");
-                    }
-                });
-                // click on time button
-                Button timeButton = (Button) findViewById(R.id.timeButton);
-                timeButton.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        setContentView(R.layout.activity_main);
-                    }
-                });
+                renderChoicePage();
             }
         });
+    }
+
+    private void renderChoicePage() {
+        // move to food/time choice page
+        setContentView(R.layout.filter_choice);
+        // click on food button
+        Button foodButton = (Button) findViewById(R.id.foodButton);
+        foodButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                renderFoodChoicePage();
+            }
+        });
+        // click on time button
+        Button timeButton = (Button) findViewById(R.id.timeButton);
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO: implement
+                setContentView(R.layout.activity_main);
+            }
+        });
+    }
+
+    private void renderFoodChoicePage() {
+        // move to food choices page
+        setContentView(R.layout.food_sort);
+
+        // populate results
+        addCard();
+
+        // add restaurant info to each restaurant
+        TextView restOne = (TextView) findViewById(R.id.restOne);
+        restOne.setText("hello hello doo doo doo");
+
+        TextView restTwo = (TextView) findViewById(R.id.restTwo);
+        restTwo.setText("beeboop");
+
+        TextView restThree = (TextView) findViewById(R.id.restThree);
+        restThree.setText("yeehaw");
+
+        TextView restFour = (TextView) findViewById(R.id.restFour);
+        restFour.setText("bing bong");
+
+        TextView restFive = (TextView) findViewById(R.id.restFive);
+        restFive.setText("achoo");
+    }
+
+    private void addCard() {
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.resultsLinearLayout);
+        CardView cardView = new CardView(this);
+        linearLayout.addView(cardView);
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams)cardView.getLayoutParams();
+        float DENSITY = cardView.getContext().getResources().getDisplayMetrics().density;
+        layoutParams.height = (int) (110.0 * DENSITY); // px to dp
+        layoutParams.width = MATCH_PARENT;
+        layoutParams.bottomMargin = (int) (15.0 * DENSITY); // px to dp
+        cardView.setLayoutParams(layoutParams);
+        cardView.setRadius(25 * DENSITY);
     }
 
     private SqliteHandler setupDB() {
