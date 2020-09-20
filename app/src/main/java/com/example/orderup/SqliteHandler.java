bb8040e1a90c2c1315c526b12c1ff6500779b266
PureWasian <alancooper509@hotmail.com>
2020-09-19 20:02:31 -0500

    added basic db implementation

---
+++ app/src/main/java/com/example/orderup/SqliteHandler.java
@@ -0,0 +1,74 @@
+package com.example.orderup;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.io.File;
+
+public class SqliteHandler {
+    private SQLiteDatabase db;
+    public String tablename = "restaurant";
+
+    public SqliteHandler() {
+        db = SQLiteDatabase.openOrCreateDatabase(":memory:", null);
+    }
+
+    public void createTable(String new_tablename, String[] colnames, String[] coltypes) {
+        String query = "CREATE TABLE IF NOT EXISTS " + new_tablename + " (";
+        query += new_tablename + "_id INTEGER PRIMARY KEY,";
+
+        // generate column name list with types
+        // TODO: currently just using TEXT for coltypes to make everything easier
+        for (int i = 0; i < colnames.length; i++) {
+            query += colnames[i] + ' ' + "TEXT"; // TEXT instaed of coltypes[i] for now;
+            if( i < colnames.length - 1) query += ", ";
+        }
+        query += ");";
+
+        // execute the statement
+        db.execSQL(query);
+    }
+
+    public void insertRow(String[] colnames, String[] rowEntry) {
+        String query = "INSERT INTO " + tablename + " (";
+
+        // generate column name list
+        for (int i = 0; i < colnames.length; i++) {
+            query += colnames[i];
+            if( i < colnames.length - 1) query += ", ";
+        }
+        query += ") VALUES (";
+
+        // generate value list
+        for (int i = 0; i < rowEntry.length; i++) {
+            query += "'" + rowEntry[i] + "'";
+            if( i < rowEntry.length - 1) query += ", ";
+        }
+        query += ");";
+
+        // execute the statement
+        db.execSQL(query);
+    }
+
+    public void printRows() {
+        String query = "SELECT * FROM " + tablename;
+        Cursor cursor = db.rawQuery(query, null);
+        while (cursor.moveToNext() ) {
+            String[] colnames = cursor.getColumnNames();
+            for (int i = 0; i < colnames.length; i++) {
+                Log.i( "orderup", cursor.getColumnName(i) + ": " + cursor.getString(i));
+            }
+            Log.i("orderup", "---------------");
+        }
+    }
+
+    public void printTables() {
+        String query = "SELECT name FROM sqlite_master WHERE type = 'table' ";
+        query += "AND name NOT LIKE 'sqlite_%' ORDER BY 1;";
+        Cursor cursor = db.rawQuery(query, null);
+        while ( cursor.moveToNext() ) {
+            Log.i("orderup", cursor.getString(0));
+        }
+    }
+}
